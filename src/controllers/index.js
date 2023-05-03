da32b10721c2c874729ba326eb85d3f20df0074c
somto2000 <somtomortune@gmail.com>
2023-05-03 17:44:04 +0100

    user management

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,18 +3,14 @@ const app = express();
 const dotenv = require("doteenv");
 const mongoose = require("mongoose");
 
-doteenv.config();
+dotenv.config();
 
 // Connect to DB
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
      console.log('connected to dbl')      
 );
 
-// import Routes
-const authRouth = require('./routes/auth');
 
-// Route Middlewares
-app.use('/api/user', authRote);
 
-app.listen(3000, () => console.log('server up and running'));
+
 
